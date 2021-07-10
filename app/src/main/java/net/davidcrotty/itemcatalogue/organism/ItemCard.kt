170559f804caeca67d9181402dbfb35a73609740
David Crotty <david.crotty@playsportsnetwork.com>
2021-07-10 19:32:41 +0100

    add positioning

--- app/src/main/java/net/davidcrotty/itemcatalogue/organism/ItemCard.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/organism/ItemCard.kt
@@ -1,39 +1,40 @@
 package net.davidcrotty.itemcatalogue.organism
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.coil.rememberCoilPainter
 import net.davidcrotty.itemcatalogue.R
 
 // TODO move to listFeature, atoms lower into a styleguide/catalogue
-// https://pbs.twimg.com/media/Eg9TpoLU8AActiA?format=jpg&name=large
+@Preview
 @Composable
-fun ItemCard() {
+fun ItemCard(item: ItemModel = ItemModel("https://pbs.twimg.com/media/Eg9TpoLU8AActiA?format=jpg&name=large")) {
     Surface(color = Color.Red) {
         Row(modifier = Modifier
             .height(160.dp)
+            .padding(16.dp)
             .fillMaxWidth()) {
             Image(
-                painter = rememberCoilPainter("https://pbs.twimg.com/media/Eg9TpoLU8AActiA?format=jpg&name=large"),
+                painter = rememberCoilPainter(item.url),
                 contentDescription = stringResource(id = R.string.list_item),
                 // will constrain its height to tow height
-                modifier = Modifier.height(240.dp) // TODO revist this
+                modifier = Modifier.aspectRatio(1f)
             )
-            Column {
+            Column(modifier = Modifier.padding(start = 16.dp)) {
                 Text("Type", maxLines = 1)
                 Text("Title", maxLines = 1)
-                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam diam turpis, tincidunt ut rhoncus nec, iaculis ac ante. Maecenas eu tempor metus. Nunc ac sapien viverra, suscipit purus nec, convallis nis")
+                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam diam turpis, tincidunt ut rhoncus nec, iaculis ac ante. Maecenas eu tempor metus. Nunc ac sapien viverra, suscipit purus nec, convallis nis", maxLines = 3)
             }
         }
     }
 }
+
+class ItemModel(val url: String)
