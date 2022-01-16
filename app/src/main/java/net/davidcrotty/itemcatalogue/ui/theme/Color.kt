4c944bd233b3968c9e210217e5c46132ba2302d5
David Crotty <david.crotty@playsportsnetwork.com>
2022-01-16 17:00:06 +0000

    add background

--- app/src/main/java/net/davidcrotty/itemcatalogue/ui/theme/Color.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/ui/theme/Color.kt
@@ -11,4 +11,5 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 fun Black800(context: Context): Color = Color(ContextCompat.getColor(context, R.color.black800))
 
-val White300 = Color(0xFDFBFA)
+val White300 = Color(0xFCFAF9)
+val White100 = Color(0xFFFFFF)
--- app/src/main/java/net/davidcrotty/itemcatalogue/ui/theme/Theme.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/ui/theme/Theme.kt
@@ -24,7 +24,9 @@ private val DarkColorPalette = darkColors(
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    surface = White300,
+    background = White100
 
     /* Other default colors to override
     background = Color.White,
