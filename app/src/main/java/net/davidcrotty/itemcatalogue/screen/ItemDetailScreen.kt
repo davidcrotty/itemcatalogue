57720c6bb950d18fd218421f4f07764bc0df08d1
David Crotty <david.crotty@playsportsnetwork.com>
2022-01-22 23:34:01 +0000

    add shadows

--- app/src/main/java/net/davidcrotty/itemcatalogue/screen/ItemDetailScreen.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/screen/ItemDetailScreen.kt
@@ -32,7 +32,7 @@ fun ItemDetailScreen(url: String = "https://cutewallpaper.org/21/dungeon-master-
             )
         }
         Column {
-            Text("Battleaxe")
+            Text("Battleaxe") // https://handstandsam.com/2021/08/09/jetpack-compose-text-shadows/ bottom shows text shadows
             Text("Damage Combat") // allcaps
             Spacer(modifier = Modifier.height(60.dp)) // TODO dynamic based on real estate
             Text("Description") // TODO behaviour based on scrolling
