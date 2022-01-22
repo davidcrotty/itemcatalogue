7d421da78bf3699704784348cbff6fb47cd3ffe4
David Crotty <david.crotty@playsportsnetwork.com>
2022-01-22 21:32:29 +0000

    add contract

--- app/src/main/java/net/davidcrotty/itemcatalogue/viewmodel/ItemsViewModel.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/viewmodel/ItemsViewModel.kt
@@ -5,19 +5,22 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import net.davidcrotty.itemcatalogue.domain.ItemRepository
 import net.davidcrotty.itemcatalogue.domain.entity.Item
+import net.davidcrotty.itemcatalogue.domain.model.ID
+import net.davidcrotty.itemcatalogue.domain.model.Query
 
-class ItemsViewModel : ViewModel() {
+class ItemsViewModel(private val itemRepository: ItemRepository) : ViewModel() {
 
     private val _items = MutableStateFlow<List<Item>>(emptyList())
 
     val items: Flow<List<Item>>
         get() = _items
 
-    fun fetchItems() {
+    fun fetchItems(id: ID) {
         viewModelScope.launch {
             _items.emit(
-                emptyList()
+                itemRepository.getItems(Query(id))
             )
         }
     }
