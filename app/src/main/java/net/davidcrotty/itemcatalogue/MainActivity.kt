dfb4d0a9bf31ecf8e12fce3c26a61de0fb0ab036
David Crotty <david.crotty@playsportsnetwork.com>
2022-01-22 20:38:34 +0000

    logic for divider

--- app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
@@ -3,20 +3,16 @@ package net.davidcrotty.itemcatalogue
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import net.davidcrotty.itemcatalogue.atom.ListDivider
+import net.davidcrotty.itemcatalogue.model.ItemModel
 import net.davidcrotty.itemcatalogue.organism.ItemCard
 import net.davidcrotty.itemcatalogue.ui.theme.CatalogueTemplateTheme
-import net.davidcrotty.itemcatalogue.ui.theme.Grey900
-import net.davidcrotty.itemcatalogue.ui.theme.ListItemTheme
-import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,16 +22,26 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     // TODO start publishing this from a viewmodel
+                    val items = listOf(
+                        ItemModel(
+                            url = "https://pbs.twimg.com/media/Eg9TpoLU8AActiA?format=jpg&name=large",
+                            type = "Type",
+                            title = "Title",
+                            description = "ffff"
+                        ),
+                        ItemModel(
+                            url = "https://pbs.twimg.com/media/Eg9TpoLU8AActiA?format=jpg&name=large",
+                            type = "Type",
+                            title = "Title",
+                            description = "ffff"
+                        ))
                     LazyColumn { // TODO template
-                        item {
-                            ItemCard()
-                        }
-                        item {
-                            ListDivider()
-                        }
-                        item {
-                            ItemCard()
-                        }
+                        itemsIndexed(items) { index, item ->
+                                ItemCard(item)
+                                if (index < items.lastIndex) {
+                                    ListDivider()
+                                }
+                            }
                         // TODO if last item dont show spacer
                     }
                 }
@@ -43,6 +49,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
