1ec1822e5a938f73c9f971c15bd891d3024ee84e
David Crotty <david.crotty@playsportsnetwork.com>
2022-01-22 21:44:52 +0000

    add real content

--- app/src/main/java/net/davidcrotty/itemcatalogue/technology/ItemMemoryDataSource.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/technology/ItemMemoryDataSource.kt
@@ -8,17 +8,17 @@ class ItemMemoryDataSource : ItemDataSource {
         return listOf( // TODO these should be DTOs
             Item(
                 id = ID(1),
-                url = "https://pbs.twimg.com/media/Eg9TpoLU8AActiA?format=jpg&name=large",
-                type = "Type",
-                title = "Title",
-                description = "1"
+                url = "https://www.scabard.com/user/Pochibella/image/10e63a407bbd6066ddb5444369e942ee.jpg",
+                type = "Weapon (sword)",
+                title = "Acheron Blade",
+                description = "The black blade of this sword is crafted from a mysterious arcane alloy. You gain a +1 bonus to attack and damage rolls made with this magic weapon. While the sword is on your person, you are immune to effects that turn undead."
             ),
             Item(
                 id = ID(2),
-                url = "https://pbs.twimg.com/media/Eg9TpoLU8AActiA?format=jpg&name=large",
-                type = "Type",
-                title = "Title",
-                description = "2"
+                url = "https://media-waterdeep.cursecdn.com/avatars/thumbnails/8/305/1000/1000/636302782994996851.jpeg",
+                type = "Weapon (battleaxe)",
+                title = "Azuredge",
+                description = "Forged by the archwizard Ahghairon, this intelligent battleaxe was crafted to defend Waterdeep. Its current wielder is a former member of Force Grey named Meloon Wardragon, but the weapon is searching for a new owner."
             )
         )
     }
