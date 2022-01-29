2eb88ccccf89ef0a491999021cc4c798317bab2f
David Crotty <david.crotty@playsportsnetwork.com>
2022-01-29 11:48:09 +0000

    upgrad compose and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
-    val compose_version by extra("1.0.5")
+    val compose_version by extra("1.1.0-rc03")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.2.0-beta01")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     }
 }
 
