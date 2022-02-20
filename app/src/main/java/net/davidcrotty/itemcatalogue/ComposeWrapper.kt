f607da124344aa1de204c9f6c670d5ad7255e550
David Crotty <david.crotty@playsportsnetwork.com>
2022-02-20 17:22:37 +0000

    tempt kludge for nav demo

--- app/src/main/java/net/davidcrotty/itemcatalogue/ComposeWrapper.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/ComposeWrapper.kt
@@ -1,19 +1,18 @@
 package net.davidcrotty.itemcatalogue
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import net.davidcrotty.itemcatalogue.di.ItemScreenGraph
 import net.davidcrotty.itemcatalogue.screen.ItemDetailScreen
 import net.davidcrotty.itemcatalogue.screen.ItemListScreen
 import net.davidcrotty.itemcatalogue.ui.theme.CatalogueTemplateTheme
 
 @Composable
-fun ComposeWrapper(itemScreenGraph: ItemScreenGraph, navigate: (path: String) -> Unit) {
+fun ComposeWrapper(navController: NavHostController, itemScreenGraph: ItemScreenGraph, navigate: (path: String) -> Unit) {
     CatalogueTemplateTheme {
-        val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "item") {
+        NavHost(navController = navController, startDestination = "itemList") {
             composable("itemList") {
                 ItemListScreen(itemScreenGraph = itemScreenGraph, navigate = { navigate(it) })
             }
--- app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
@@ -20,8 +20,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeWrapper(ItemScreenGraphImpl()) {
-                // navigator.navigate(it)
+            val controller = rememberNavController()
+            val navigator = NavigatorImpl(controller)
+            ComposeWrapper(controller, ItemScreenGraphImpl()) {
+                 navigator.navigate(it)
             }
         }
     }
