0464eea45e0e4b4dc668170a4e46a5e991b7bfa1
David Crotty <david.crotty@playsportsnetwork.com>
2022-04-02 13:55:24 +0100

    wip: begin adding groovy and enabling junit5 tess

---
+++ app/src/test/java/net/davidcrotty/itemcatalogue/viewmodel/ItemsViewModelTest.kt
@@ -0,0 +1,11 @@
+package net.davidcrotty.itemcatalogue.viewmodel
+
+import org.junit.jupiter.api.Test
+
+internal class ItemsViewModelTest {
+
+    @Test
+    fun when_fetching_items() {
+
+    }
+}
