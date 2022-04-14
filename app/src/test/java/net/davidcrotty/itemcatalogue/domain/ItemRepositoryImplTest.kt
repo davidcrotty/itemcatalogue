413e9f2e2a7238e091d8a096629c30c099fd57c2
David Crotty <david.crotty@playsportsnetwork.com>
2022-04-14 20:19:09 +0100

    failing fetch test

---
+++ app/src/test/java/net/davidcrotty/itemcatalogue/domain/ItemRepositoryImplTest.kt
@@ -0,0 +1,42 @@
+package net.davidcrotty.itemcatalogue.domain
+
+import fr.xgouchet.elmyr.Forge
+import io.mockk.mockk
+import net.davidcrotty.itemcatalogue.items.entity.ID
+import net.davidcrotty.itemcatalogue.items.entity.Item
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class ItemRepositoryImplTest {
+
+    private val forge = Forge()
+
+    @Test
+    fun `when retrieving items from remote server`() {
+
+        // Given an available remote list of items items
+        val expectedItems = listOf(
+            Item(
+                id = ID(forge.anInt()),
+                url = forge.aString(),
+                type = forge.aString(),
+                title = forge.aString(),
+                description = forge.aString()
+            )
+        )
+        val sut = ItemRepositoryImpl(
+            mockk()
+        )
+
+        // when fetching items
+        val items = sut.getItems()
+
+        // Then should return items
+        assertEquals(expectedItems, items)
+    }
+
+    @Test
+    fun `when failing to retrieve items from remote server`() {
+
+    }
+}
