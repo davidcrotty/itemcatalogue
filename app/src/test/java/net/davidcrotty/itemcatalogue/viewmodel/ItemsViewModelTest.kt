a88a7f37cb6f936a21cbec8ed251ee4201d70f43
David Crotty <david.crotty@playsportsnetwork.com>
2022-04-14 13:37:50 +0100

    red test for item fetching

--- app/src/test/java/net/davidcrotty/itemcatalogue/viewmodel/ItemsViewModelTest.kt
+++ app/src/test/java/net/davidcrotty/itemcatalogue/viewmodel/ItemsViewModelTest.kt
@@ -1,9 +1,15 @@
 package net.davidcrotty.itemcatalogue.viewmodel
 
 import io.mockk.mockk
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
 import net.davidcrotty.itemcatalogue.helpers.CoroutineTest
+import net.davidcrotty.itemcatalogue.items.entity.ID
+import net.davidcrotty.itemcatalogue.items.entity.Item
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class ItemsViewModelTest : CoroutineTest {
@@ -12,15 +18,30 @@ internal class ItemsViewModelTest : CoroutineTest {
     override var dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
 
     @Test
-    fun when_fetching_items() {
-        // Given a valid item
+    fun when_fetching_items() = runBlockingTest {
+        // Given a valid item list
+        val itemList = listOf(
+            Item(
+                id = ID(1),
+                url = "",
+                type = "",
+                title = "",
+                description = ""
+            )
+        )
         val sut = ItemsViewModel(
             mockk()
         )
 
         // When fetching items
+        val results = mutableListOf<List<Item>>()
         sut.fetchItems()
+        val job = launch {
+            sut.items.toList(results)
+        }
 
         // Then should render item
+        assertEquals(itemList, results)
+        job.cancel()
     }
 }
