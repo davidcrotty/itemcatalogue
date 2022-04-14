13ea15ed8eb2217284b246c0d8446ea4fc2355a4
David Crotty <david.crotty@playsportsnetwork.com>
2022-04-14 16:03:33 +0100

    adds usecase test

--- domain/items/src/main/java/net/davidcrotty/itemcatalogue/items/usecase/GetFeedUseCaseImpl.kt
+++ domain/items/src/main/java/net/davidcrotty/itemcatalogue/items/usecase/GetFeedUseCaseImpl.kt
@@ -1,9 +1,11 @@
 package net.davidcrotty.itemcatalogue.items.usecase
 
+import net.davidcrotty.itemcatalogue.items.entity.ID
 import net.davidcrotty.itemcatalogue.items.entity.Item
+import net.davidcrotty.itemcatalogue.items.repository.ItemRepository
 
-class GetFeedUseCaseImpl : GetFeedUsecase {
+class GetFeedUseCaseImpl(private val itemRepository: ItemRepository) : GetFeedUsecase {
     override fun getFeed(): List<Item> {
-        TODO("Not yet implemented")
+        return itemRepository.getItems()
     }
 }
--- domain/items/src/test/java/net/davidcrotty/itemcatalogue/items/usecase/GetFeedUseCaseImplTest.kt
+++ domain/items/src/test/java/net/davidcrotty/itemcatalogue/items/usecase/GetFeedUseCaseImplTest.kt
@@ -1,12 +1,40 @@
 package net.davidcrotty.itemcatalogue.items.usecase
 
+import fr.xgouchet.elmyr.Forge
+import io.mockk.every
+import io.mockk.mockk
+import net.davidcrotty.itemcatalogue.items.entity.ID
+import net.davidcrotty.itemcatalogue.items.entity.Item
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class GetFeedUseCaseImplTest {
-    @Test
-    fun `when retrieving an item feed`() {
-        val sut = GetFeedUseCaseImpl()
 
-        sut.getFeed()
+    private val forge = Forge()
+
+    @Test
+    fun `when retrieving an item list`() {
+        // Given a list of items
+        val expectedItems = listOf(
+            Item(
+                id = ID(1),
+                url = forge.aString(),
+                type = forge.aString(),
+                title = forge.aString(),
+                description = forge.aString()
+            )
+        )
+
+        val sut = GetFeedUseCaseImpl(
+            mockk {
+                every { getItems() } returns expectedItems
+            }
+        )
+
+        // when fetching an item feed
+        val items = sut.getFeed()
+
+        // Then feed should be delivered
+        assertEquals(expectedItems, items)
     }
 }
