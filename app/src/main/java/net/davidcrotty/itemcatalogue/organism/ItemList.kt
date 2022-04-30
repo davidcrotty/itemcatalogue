d5c41eaacae11140515f8ea337d85ee81e992bbe
David Crotty <david.crotty@playsportsnetwork.com>
2022-04-30 12:22:16 +0100

    center loading indicator

--- app/src/main/java/net/davidcrotty/itemcatalogue/organism/ItemList.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/organism/ItemList.kt
@@ -3,6 +3,7 @@ package net.davidcrotty.itemcatalogue.organism
 import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import net.davidcrotty.itemcatalogue.atom.ListDivider
@@ -12,31 +13,33 @@ import net.davidcrotty.itemcatalogue.model.FeedItem
 fun ItemList(itemList: List<FeedItem>,
              navigate:((path: String) -> Unit)? = null,
              fetchMore: (() -> Unit)? = null) {
-    val listState = rememberLazyListState()
-    Log.d("ListTemplate", "item size: ${itemList.size}")
+    Surface {
+        val listState = rememberLazyListState()
+        Log.d("ListTemplate", "item size: ${itemList.size}")
 
-    LazyColumn {
-        itemList.forEachIndexed { index, dungeonItem ->
-            item {
-                ItemCard(dungeonItem, onClick = {
-                    navigate?.invoke("item")
-                })
-            }
-
-            // TODO factory to abstract render details
-            if (index < itemList.lastIndex) {
+        LazyColumn {
+            itemList.forEachIndexed { index, dungeonItem ->
                 item {
-                    ListDivider()
+                    ItemCard(dungeonItem, onClick = {
+                        navigate?.invoke("item")
+                    })
                 }
+
+                // TODO factory to abstract render details
+                if (index < itemList.lastIndex) {
+                    item {
+                        ListDivider()
+                    }
+                }
+
             }
-
         }
-    }
 
-    // TODO should this logic be abstracted?
-    if (listState.layoutInfo.visibleItemsInfo.isEmpty()) {
-        LaunchedEffect(key1 = itemList) {
-            fetchMore?.invoke()
+        // TODO should this logic be abstracted?
+        if (listState.layoutInfo.visibleItemsInfo.isEmpty()) {
+            LaunchedEffect(key1 = itemList) {
+                fetchMore?.invoke()
+            }
         }
     }
 }
--- app/src/main/java/net/davidcrotty/itemcatalogue/template/ListTemplate.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/template/ListTemplate.kt
@@ -3,6 +3,7 @@ package net.davidcrotty.itemcatalogue.template
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import net.davidcrotty.itemcatalogue.atom.LoadingIndicator
 import net.davidcrotty.itemcatalogue.model.FeedItem
 import net.davidcrotty.itemcatalogue.organism.ItemList
@@ -12,12 +13,10 @@ fun ListTemplate(itemList: List<FeedItem>,
                  isLoading: Boolean,
                  navigate:((path: String) -> Unit)? = null,
                  fetchMore: (() -> Unit)? = null) {
-    Surface {
-        Column {
-            ItemList(itemList, navigate, fetchMore)
-            if (isLoading) {
-                LoadingIndicator()
-            }
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        ItemList(itemList, navigate, fetchMore)
+        if (isLoading) {
+            LoadingIndicator()
         }
     }
 }
