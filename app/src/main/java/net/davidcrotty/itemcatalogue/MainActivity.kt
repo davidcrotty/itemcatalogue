9135943f382a446634decba71f0e4c344dfeefcd
David Crotty <david.crotty@playsportsnetwork.com>
2022-07-24 16:06:35 +0100

    working splash

--- app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
@@ -1,6 +1,8 @@
 package net.davidcrotty.itemcatalogue
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
@@ -19,35 +21,59 @@ import net.davidcrotty.itemcatalogue.technology.navigation.NavigatorImpl
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private val dndContainer by lazy { applicationContext.applicationContext as DndCatalogueAppContainer }
 
+    private val viewModel = LaunchViewModel()
+
+    class LaunchViewModel : ViewModel() {
+
+        var isReady = false
+
+        fun loadFeed() {
+            viewModelScope.launch {
+                delay(5000)
+                isReady = true
+            }
+        }
+
+    }
+
     @OptIn(ExperimentalAnimationGraphicsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        viewModel.loadFeed()
         setContent {
+            // Set up an OnPreDrawListener to the root view.
+            val content: View = findViewById(android.R.id.content)
+            content.viewTreeObserver.addOnPreDrawListener(
+                object : ViewTreeObserver.OnPreDrawListener {
+                    override fun onPreDraw(): Boolean {
+                        // Check if the initial data is ready.
+                        return if (viewModel.isReady) {
+                            // The content is ready; start drawing.
+                            content.viewTreeObserver.removeOnPreDrawListener(this)
+                            true
+                        } else {
+                            // The content is not ready; suspend.
+                            false
+                        }
+                    }
+                }
+            )
             val controller = rememberNavController()
             val navigator = NavigatorImpl(controller)
-//            Box(
-//                modifier = Modifier
-//                    .fillMaxHeight()
-//                    .fillMaxWidth()
-//            ) {
-//                val image = AnimatedImageVector.animatedVectorResource(R.drawable.sword)
-//                val atEnd by remember { mutableStateOf(false) }
-//                Icon(
-//                    painter = rememberAnimatedVectorPainter(image, atEnd),
-//                    contentDescription = null // decorative element
-//                )
-//            }
-//            ComposeWrapper(
-//                controller, dndContainer.itemScreenGraph()
-//            ) {
-//                navigator.navigate(it)
-//            }
+            ComposeWrapper(
+                controller, dndContainer.itemScreenGraph()
+            ) {
+                navigator.navigate(it)
+            }
         }
     }
 }
