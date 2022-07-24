fc17b0af0e965cc4a2def8c7a6351f0275826d6f
David Crotty <david.crotty@playsportsnetwork.com>
2022-07-24 17:32:51 +0100

    rename variables

--- app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
@@ -1,26 +1,12 @@
 package net.davidcrotty.itemcatalogue
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
-import androidx.compose.animation.graphics.res.animatedVectorResource
-import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
-import androidx.compose.animation.graphics.vector.AnimatedImageVector
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Icon
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import net.davidcrotty.itemcatalogue.di.DndCatalogueAppContainer
 import net.davidcrotty.itemcatalogue.technology.navigation.NavigatorImpl
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -35,12 +21,12 @@ class MainActivity : ComponentActivity() {
 
     class LaunchViewModel : ViewModel() {
 
-        var isReady = false
+        var dismissSplashScreen = true
 
         fun loadFeed() {
             viewModelScope.launch {
                 delay(5000)
-                isReady = true
+                dismissSplashScreen = false
             }
         }
 
@@ -48,27 +34,11 @@ class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalAnimationGraphicsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { viewModel.dismissSplashScreen }
         viewModel.loadFeed()
         setContent {
-            // Set up an OnPreDrawListener to the root view.
-            val content: View = findViewById(android.R.id.content)
-            content.viewTreeObserver.addOnPreDrawListener(
-                object : ViewTreeObserver.OnPreDrawListener {
-                    override fun onPreDraw(): Boolean {
-                        // Check if the initial data is ready.
-                        return if (viewModel.isReady) {
-                            // The content is ready; start drawing.
-                            content.viewTreeObserver.removeOnPreDrawListener(this)
-                            true
-                        } else {
-                            // The content is not ready; suspend.
-                            false
-                        }
-                    }
-                }
-            )
             val controller = rememberNavController()
             val navigator = NavigatorImpl(controller)
             ComposeWrapper(
