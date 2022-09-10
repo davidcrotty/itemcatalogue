3a0bbe80c66038ab776b7b07ee5862479aa468f6
David Crotty <david.crotty@playsportsnetwork.com>
2022-09-10 22:08:16 +0100

    add connectivity localisation capability

--- app/src/main/java/net/davidcrotty/itemcatalogue/organism/ErrorDialog.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/organism/ErrorDialog.kt
@@ -4,18 +4,21 @@ import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import net.davidcrotty.itemcatalogue.R
 
 @Composable
 fun PreloadApplicationErrorDialog() {
-    AlertDialog(onDismissRequest = {  },
+    AlertDialog(onDismissRequest = { // noop don't want to dismiss
+        },
         title = {
-            Text(text = "Unable to launch application", modifier = Modifier.semantics { contentDescription = "unable to launch application heading" })
+            Text(text = stringResource(id = R.string.dialog_title_unable_to_launch_application), modifier = Modifier.semantics { contentDescription = "unable to launch application heading" })
         },
         text = {
             // TODO pass reasons network or unknown error
-            Text(text = "Please check network connectivity and try again", modifier = Modifier.semantics { contentDescription = "connectivity error" })
+            Text(text = stringResource(id = R.string.dialog_description_connectivity_error), modifier = Modifier.semantics { contentDescription = "connectivity error" })
         },
         buttons = {})
 }
