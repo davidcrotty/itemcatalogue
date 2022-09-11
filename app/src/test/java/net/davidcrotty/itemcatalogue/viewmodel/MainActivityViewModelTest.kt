63c9a986cd1dc22d80020d0565c5485dac446521
David Crotty <david.crotty@playsportsnetwork.com>
2022-09-11 12:12:36 +0100

    update test to validate behaviour

--- app/src/test/java/net/davidcrotty/itemcatalogue/viewmodel/MainActivityViewModelTest.kt
+++ app/src/test/java/net/davidcrotty/itemcatalogue/viewmodel/MainActivityViewModelTest.kt
@@ -1,35 +1,47 @@
 package net.davidcrotty.itemcatalogue.viewmodel
 
+import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import net.davidcrotty.itemcatalogue.helpers.CoroutineTest
+import net.davidcrotty.itemcatalogue.helpers.TestCoroutineExtension
 import net.davidcrotty.itemcatalogue.items.model.PreloadStatus
+import net.davidcrotty.itemcatalogue.model.ApplicationLoadState
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
-class MainActivityViewModelTest {
+class MainActivityViewModelTest : CoroutineTest {
+
+    override var testScheduler: TestCoroutineScheduler = TestCoroutineScheduler()
 
     @Test
     fun `test preloading application success`() {
         val sut = MainActivityViewModel(
             preloadUseCase = mockk {
-                every { execute() } returns PreloadStatus.Loaded
+                coEvery { execute() } returns PreloadStatus.Loaded
             }
         )
 
         sut.preloadApplication()
+        testScheduler.advanceUntilIdle()
 
         assertEquals(false, sut.shouldKeepSplashOnScreen())
+        assertEquals(false, sut.launchErrorDialogShown.value)
     }
 
     @Test
     fun `test preloading application failure`() {
         val sut = MainActivityViewModel(
             preloadUseCase = mockk {
-                every { execute() } returns PreloadStatus.Error
+                coEvery { execute() } returns PreloadStatus.Error
             }
         )
 
         sut.preloadApplication()
+        testScheduler.advanceUntilIdle()
 
         assertEquals(false, sut.shouldKeepSplashOnScreen())
         assertEquals(true, sut.launchErrorDialogShown.value)
