52c0d48e51e568920950fcd9fd87a5357e52591e
David Crotty <david.crotty@playsportsnetwork.com>
2022-11-13 23:59:26 +0000

    add coil

---
+++ app/src/main/java/net/davidcrotty/itemcatalogue/molecule/DetailImage.kt
@@ -0,0 +1,11 @@
+package net.davidcrotty.itemcatalogue.molecule
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.ImagePainter
+
+@Preview
+@Composable
+fun DetailImage() {
+//    AsyncImage(painter = ImagePainter(), contentDescription = "Detail Image")
+}
