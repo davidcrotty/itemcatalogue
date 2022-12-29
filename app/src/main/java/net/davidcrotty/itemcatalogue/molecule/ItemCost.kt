28b8c7bff345201f1270fa7f8bb49a6c2fa484d0
David Crotty <david.crotty@playsportsnetwork.com>
2022-12-29 18:38:53 +0000

    adds coin rendering

--- app/src/main/java/net/davidcrotty/itemcatalogue/molecule/ItemCost.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/molecule/ItemCost.kt
@@ -1,13 +1,21 @@
 package net.davidcrotty.itemcatalogue.molecule
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.davidcrotty.itemcatalogue.R
 
 
@@ -25,13 +33,23 @@ fun ItemCost(
 ) {
     val itemCost = stringResource(id = R.string.item_cost)
     val goldAmount = stringResource(id = R.string.gold_cost, formatArgs = arrayOf(gold))
-    Row(Modifier.semantics {
-        contentDescription = itemCost
-    }) {
-        Row(Modifier.semantics {
-            contentDescription = goldAmount
-        }) {
-            Text(text = "G: $silver")
+    Row(
+        Modifier
+            .semantics {
+                contentDescription = itemCost
+            }
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(
+            Modifier.semantics {
+                contentDescription = goldAmount
+            },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.crown_coin), null)
+            Text(text = "$gold")
         }
         Text(text = "S: $silver")
         Text(text = "C: $copper")
