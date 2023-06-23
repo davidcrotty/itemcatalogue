f533dc6b24f2ff887e72369a92b8b551123120b3
davidcrotty <david.crotty@playsportsnetwork.com>
2023-06-23 17:19:25 +0100

    create new module

--- app/src/main/java/net/davidcrotty/itemcatalogue/screen/ItemDetailScreen.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/screen/ItemDetailScreen.kt
@@ -2,18 +2,12 @@ package net.davidcrotty.itemcatalogue.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
-import net.davidcrotty.itemcatalogue.R
 import net.davidcrotty.itemcatalogue.atom.DividerLabel
 import net.davidcrotty.itemcatalogue.molecule.DetailImage
 import net.davidcrotty.itemcatalogue.template.ItemDetailTemplate
