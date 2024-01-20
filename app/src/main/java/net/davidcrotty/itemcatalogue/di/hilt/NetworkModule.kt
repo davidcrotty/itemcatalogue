5770c9d8e747ed3e3ae9d1abc03afefeee6ef0ea
David Crotty <david.crotty.work@gmail.com>
2024-01-20 12:34:19 +0000

    extract, rotate and secure api key

--- app/src/main/java/net/davidcrotty/itemcatalogue/di/hilt/NetworkModule.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/di/hilt/NetworkModule.kt
@@ -1,19 +1,27 @@
 package net.davidcrotty.itemcatalogue.di.hilt
 
+import android.content.Context
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import net.davidcrotty.itemcatalogue.R
 import net.davidcrotty.itemcatalogue.data.item.api.ItemAPI
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Named("apiKey")
+    fun provideApiKey(@ApplicationContext context: Context): String = context.getString(R.string.dnd_tools_api_key)
 
     @Provides
     fun provideItemAPI(okHttpClient: OkHttpClient, moshi: Moshi): ItemAPI {
--- data/src/main/java/net/davidcrotty/itemcatalogue/data/item/RemoteItemDataSourceImpl.kt
+++ data/src/main/java/net/davidcrotty/itemcatalogue/data/item/RemoteItemDataSourceImpl.kt
@@ -10,22 +10,21 @@ import net.davidcrotty.itemcatalogue.data.item.exception.ContentNotFound
 import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
+import javax.inject.Named
 
 class RemoteItemDataSourceImpl @Inject constructor(
     private val itemAPI: ItemAPI,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    @Named("apiKey")
+    private val apiKey: String = ""
 ) : RemoteItemDataSource {
-
-    // TODO figure out how to make multiple tokens with shorter expiry's
-    private val apiToken =
-        "eyJhbGciOiJSUzI1NiIsImtpZCI6IjcyOTE4OTQ1MGQ0OTAyODU3MDQyNTI2NmYwM2U3MzdmNDVhZjI5MzIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIzMjU1NTk0MDU1OS5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsImF1ZCI6IjMyNTU1OTQwNTU5LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic3ViIjoiMTAxMjU2NTc5MjEwMTUyOTIxMjQ4IiwiZW1haWwiOiJkYXZpZGNyb3R0eXdvcmtAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJELU55V1BuM2RTZmFQaEhYazZzb293IiwiaWF0IjoxNjQ3ODA3ODA0LCJleHAiOjE2NDc4MTE0MDR9.CPf-NVaEljIdTMF48bx2VB9K1uKVQnF-gUZpkylAxP58JNZK5DRbK6FW1GfAKZoMZVHtR4Xz48jkdq5gXXsM-WfqSTmiXBsKEGk84gkYWtwAG-3h-ObDmEq3L75FPHdloT0eoXqMAM57M9JvTmiDmWtSkJF6puNqFUEK8TITE8JTjkhxWZ_-JYiHsPi1FDZZT03zVlzqs-Gx1--o8OXww52Q7y8F5z5_75RDvV7qf6MKnq3q4Nyjc27qsSZv0Tfri5QZfgVz3QFnu6e-K1LsH8VuCZf7J3idvDpIc6FIlK-tFzR0lG0xythi7ImTEdkIxgdFgdjWZ09YC36KmzJE3A"
 
     // TODO make into object for queries
     override suspend fun fetchAfter(id: String?, limit: Int): List<ItemDTO> {
         val items = withContext(dispatcher) {
             try {
                 // TODO log trace ids for logging
-                itemAPI.getItems(apiToken, limit, id)
+                itemAPI.getItems(apiKey, limit, id)
             } catch (e: HttpException) {
                 val httpCode = e.code()
                 if (httpCode == 404 || httpCode == 500) {
