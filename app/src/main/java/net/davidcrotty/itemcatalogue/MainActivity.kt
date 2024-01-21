6f10255ff8e02e9f44f5c9323d603da7168b40dd
David Crotty <david.crotty.work@gmail.com>
2024-01-21 19:54:40 +0000

    wip ui state

--- app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/MainActivity.kt
@@ -6,25 +6,28 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.LocalContentColor
-import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
+import net.davidcrotty.itemcatalogue.organism.ItemCatalogueAppBar
 import net.davidcrotty.itemcatalogue.organism.PreloadApplicationErrorDialog
 import net.davidcrotty.itemcatalogue.screen.ItemListScreen
 import net.davidcrotty.itemcatalogue.technology.navigation.NavigationGraph
-import net.davidcrotty.itemcatalogue.theme.Black800
 import net.davidcrotty.itemcatalogue.theme.CatalogueTemplateTheme
 import net.davidcrotty.itemcatalogue.theme.LocalCatalogTemplateValues
-import net.davidcrotty.itemcatalogue.theme.LocalColors
 import net.davidcrotty.itemcatalogue.viewmodel.MainActivityViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -49,16 +52,55 @@ class MainActivity : ComponentActivity() {
                     if (showErrorDialog) {
                         PreloadApplicationErrorDialog()
                     } else {
-                        val controller = rememberNavController()
-                        NavigationGraph(
-                            controller = controller,
-                            itemListScreenFactory = {
-                                ItemListScreen { controller.navigate(it) }
-                            }
-                        )
+                        AppTemplate()
                     }
                 }
             }
         }
     }
+
+    class AppStateHolder(
+        var appTitle: String,
+        var appBarOverlayContent: Boolean
+    )
+
+    @Composable
+    fun AppTemplate() {
+        var appTitle by remember { mutableStateOf("") }
+
+        if (false) {
+            Box {
+                val controller = rememberNavController()
+                NavigationGraph(
+                    controller = controller,
+                    itemListScreenFactory = {
+                        ItemListScreen { controller.navigate(it) }
+                    },
+                    { appTitle = it },
+                    { false }
+                )
+                ItemCatalogueAppBar(title = appTitle)
+            }
+        } else {
+            Column {
+                ItemCatalogueAppBar(title = appTitle)
+                val controller = rememberNavController()
+                NavigationGraph(
+                    controller = controller,
+                    itemListScreenFactory = {
+                        ItemListScreen { controller.navigate(it) }
+                    },
+                    { appTitle = it },
+                    { false }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun rememberAppScreenState(
+    appState: MainActivity.AppStateHolder
+): MainActivity.AppStateHolder = remember {
+    appState
 }
--- app/src/main/java/net/davidcrotty/itemcatalogue/technology/navigation/NavigationGraph.kt
+++ app/src/main/java/net/davidcrotty/itemcatalogue/technology/navigation/NavigationGraph.kt
@@ -1,7 +1,5 @@
 package net.davidcrotty.itemcatalogue.technology.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -13,29 +11,29 @@ import androidx.navigation.navArgument
 import net.davidcrotty.itemcatalogue.R
 import net.davidcrotty.itemcatalogue.detailscreen.presentation.ItemDetailViewModel
 import net.davidcrotty.itemcatalogue.detailscreen.ui.screen.ItemDetailScreen
-import net.davidcrotty.itemcatalogue.organism.ItemCatalogueAppBar
 
 @Composable
 fun NavigationGraph(
     controller: NavHostController,
-    itemListScreenFactory: @Composable () -> Unit
+    itemListScreenFactory: @Composable () -> Unit,
+    titleChange: (String) -> Unit,
+    overlayChange: (Boolean) -> Unit
 ) {
-    Column {
-        Surface {
-            ItemCatalogueAppBar(title = stringResource(id = R.string.listing_title))
+    NavHost(navController = controller, startDestination = "itemList") {
+        composable("itemList") {
+            titleChange(stringResource(id = R.string.listing_title))
+            overlayChange(false)
+            itemListScreenFactory()
         }
-        NavHost(navController = controller, startDestination = "itemList") {
-            composable("itemList") {
-                itemListScreenFactory()
-            }
-            composable("item/{itemId}", arguments = listOf(navArgument("itemId") {
-                type = NavType.StringType
-                nullable = false
-            })) { _ ->
-                ItemDetailScreen(
-                    detailViewModel = hiltViewModel() as ItemDetailViewModel
-                )
-            }
+        composable("item/{itemId}", arguments = listOf(navArgument("itemId") {
+            type = NavType.StringType
+            nullable = false
+        })) { _ ->
+            titleChange("")
+            overlayChange(true)
+            ItemDetailScreen(
+                detailViewModel = hiltViewModel() as ItemDetailViewModel
+            )
         }
     }
 }
